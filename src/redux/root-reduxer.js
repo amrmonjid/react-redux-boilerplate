da013b6551e349d340ce1569e7731777288c1121
Amr Monjid <amr.monjid@gmail.com>
2019-11-04 22:32:02 +0100

    fix the reducer import

--- src/redux/root-reduxer.js
+++ src/redux/root-reduxer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import sectionReducer from "./smaple-section/section.reducer";
+import sectionReducer from "./sample-section/section.reducer";
 
 export default combineReducers({
   section: sectionReducer
